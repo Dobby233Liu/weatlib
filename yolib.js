29053f7b6b69f877d2b1a919f829982558229b7f
Tent <yoyyay318@gmail.com>
2018-03-01 18:48:24 +0100

    Create yolib.js

---
+++ yolib.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+var data = require('./yolib.txt');
+
+http.createServer(function(req, res){
+    fs.readFile('local.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
