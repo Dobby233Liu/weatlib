f1d71bc0a747e3d23032eef8000452601afe187e
Tent <yoyyay318@gmail.com>
2018-03-01 19:09:26 +0100

    :library: patch

--- yolib.js
+++ yolib.js
@@ -3,8 +3,10 @@ var fs = require('fs');
 var data = require('./config.json');
 
 var go = function();
+// start function
 http.createServer(function(req, res){
     fs.readFile('local.html',function (err, data){
+        // read file
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
